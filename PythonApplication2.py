d3e46f8337f9c6229819395d3b313ce4153a687d
Colin Gideon <colingideon360@gmail.com>
2018-06-20 06:22:50 -0700

    Got something done for once
    # CHANGE LOG
    
    Added in *stop* to prevent code from breaking due to blank name
    Added a debug mode, accessible by entering debug into name creation slot
    Added time.sleep commands between dialog, in an attempt to help dialog to flow smoothly. (W.I.P)

--- PythonApplication2.py
+++ PythonApplication2.py
@@ -1,10 +1,16 @@
+import time
 print("""Hello, and Welcome to Monty Python and the Holy Grail: Command Prompt Edition! Please start by entering your character 
 data.""")
 
 
 #Character Creation
 username = input("Name: ")
-if len(username) > 0:
+if str(username) == "debug":
+    print ("Entered debugging mode. Name defaulted to Faggotoli the Great.")
+    username = "Faggotoli"
+    usernameprefix = "the Great"
+    time.sleep(2)
+elif len(username) > 0:
     userchecking = input("Ok, so your name is " + username + ", right? Y or N ")
     usercheck = str.lower(userchecking)
     if usercheck == "y":
@@ -12,16 +18,19 @@ if len(username) > 0:
     elif usercheck == "n":
         username = input("Okay, what would you like for your username to be instead? ")
         print ("Okay, your new username will be " + username)
-        usernameprefix = input("Ok! What prefix would you like to be attached to your name? ")
+        usernameprefix = input("Ok! What suffix would you like to be attached to your name? ")
     else:
         print("Congrats. your name is now Code Mc Breaky, dick.")
         username = "Code"
         usernameprefix = "Mc Breaky"
 else:
-    print ("NO BREAKING MY CODE. ONLY I AM ALLOWED TO BRICK THE CODE.")
+    exit("NO BREAKING MY CODE. ONLY I AM ALLOWED TO BRICK THE CODE.")
 user = username + " " + usernameprefix
 print("Ok! your final name shall be " + user)
 input("Hit enter to progress into the story.")
+
+#Code Seperator
+
 print(" ")
 print(" ")
 print(" ")
@@ -56,16 +65,24 @@ print(" ")
 print(" ")
 
 #Start of scenarios
+
+
+
 print("Whilst traveling through a forest, " + user + """ and Patsy Discover two armourclad men fighting eachother, both with their
 faces covered. As the fight ensues, one of the men, dressed in black armour defeats the other, killing him while he does so. As
 he is victorious he retreives his sword and stands tall. What do you do?""")
+time.sleep(7)
 blacknight1 = input("Do you leave, or do you approach the armed man? ")
 blackknight1 = str.lower(blacknight1)
 if blackknight1 == "approach":
     print("You chose to approach the man. As " + user + " approaches, he praises the man for his victory.")
+    time.sleep(1)
 elif blackknight1 == "leave":
     print("You chose to leave the forest, as to not anger the man and end up in the same situation as the man before you")
+    time.sleep(1)
 elif blackknight1 == "hand him the communist manifesto":
     print("You have solved the worlds problems. Congradulations! Keep in mind, you didn't win, WE won.")
+    time.sleep(5)
 else:
     print("You chose to stare blankly at the man and not to anything. How curious?")
+    time.sleep(5)
